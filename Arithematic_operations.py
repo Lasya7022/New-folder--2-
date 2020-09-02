fc0dae4d78f64a57180ac26ddfe77462b39dfc44
Lasya7022 <githublasyacc7022@gmail.com>
2020-09-02 22:21:06 +0530

    Arithop

---
+++ Arithematic_operations.py
@@ -0,0 +1,16 @@
+n1 = int(input('Enter First number: '))
+n2 = int(input('Enter Second number '))
+add = n1 + n2
+print('Sum of ',n1 , '&' ,n2 ,'is :',add)
+dif = n1 - n2
+print('Difference of ',n1 ,'&' ,n2 ,'is :',dif)
+mul = n1 * n2
+print('Product of' ,n1 ,'&' ,n2 ,'is :',mul)
+div = n1 / n2
+print('Division of ',n1 ,'&' ,n2 ,'is :',div)
+floor_div = n1 // n2
+print('Floor Division of ',n1 ,'&' ,n2 ,'is :',floor_div)
+power = n1 ** n2
+print('Exponent of ',n1 ,'&' ,n2 ,'is :',power)
+modulus = n1 % n2
+print('Modulus of ',n1 ,'&' ,n2 ,'is :',modulus)
